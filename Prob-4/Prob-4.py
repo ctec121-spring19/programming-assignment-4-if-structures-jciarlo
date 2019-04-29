db85d24d0a0077ce99b26715088706665152a34c
Jon Ciarlo <j.ciarlo@students.clark.edu>
2019-04-29 13:48:33 -0700

    Prob 4 and 5

--- Prob-4/Prob-4.py
+++ Prob-4/Prob-4.py
@@ -2,7 +2,7 @@
 #   Programming Assignment 4
 #     Prob-4.py
 
-# <YOUR NAME>
+# Jonathan Ciarlo
 
 # Author: Bruce Elgort
 # Date: July 12, 2017
@@ -16,28 +16,34 @@ the shipping cost is waived. Write a program that
 calculates the cost of an order. Name your function
 coffeeProcessor()
 """
-
-def coffeeProcessor():
+#Added quotation to line 27
+#Added a second parenthesis to line 27
+#Add colon to line 35 after else
+#Add quotation to line 43 after, "The cost of the order is:"
+#Line 24 add go before coffeeProessor
+#Change Evaluate to eval line 32
+#line 42 change quntity to quantity 
+def gocoffeeProcessor():
 
     # define variables
     overHead = 1.25
     priceOfCoffee = 16.50
 
     # get number of pounds from user
-    quantity = evaluate(input("How many pounds of coffee would you like to order? )
+    quantity = eval(input("How many pounds of coffee would you like to order?"))
     
     # Check number of pounds ordered
     # If less than or equal to 10 pounds we must charge for shipping
     if quantity <= 10:
         shippingPerPound = .76
-    else
+    else:
         shippingPerPound = 0      
 
     # Calculate cost of order
-    costOfOrder = (quantity * priceOfCoffee) + (quntity * shippingPerPound) + overHead
+    costOfOrder = (quantity * priceOfCoffee) + (quantity * shippingPerPound) + overHead
 
     # Print result
-    print(The cost of the order is:",costOfOrder)
+    print("The cost of the order is:",costOfOrder)
 
 # start the program
 gocoffeeProcessor()
--- Prob-5/Prob-5.py
+++ Prob-5/Prob-5.py
@@ -2,10 +2,20 @@
 #   Programming Assignment 4
 #     Prob-5.py
 
-# <YOUR NAME>
+# Jonathan Ciarlo
 
 def main():
-    x = eval(2)
+    try:
+         x = eval(2)
     print("x:", x)
+    except TypeError:
+        print("Type Error, Sorry :(")
+    except:
+        print("unknown error, you hacked the matrix :(")
+                
+
+
 
 main()
+   
+                
