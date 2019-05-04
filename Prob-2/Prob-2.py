60da966d6c67224e9b89d2289a1c9a8bc88373f2
Jon Ciarlo <j.ciarlo@students.clark.edu>
2019-05-04 14:05:33 -0700

    Problems 2&3 - finished

--- Prob-2/Prob-2.py
+++ Prob-2/Prob-2.py
@@ -2,14 +2,61 @@
 #   Programming Assignment 4
 #     Prob-2.py
 
-# <YOUR NAME>
+# Jonathan Ciarlo
 
 '''
 Your IPO comment goes here
+#inputs: persons name, persons hourly wage, persons hours
+#process: assign variables (name, wage, hours), print out that information with the assigned parameters i.e. overtime = wages times 1.5 if hours over 40
+#outputs: name, reg wages, ovetime wages, total wages, tax withholding, Insurance withholding, take home pay
+
+
+
+
 '''
 
 def main():
     # your code goes here
+    import math
+   #Inputs
+    
+    #variables
+    name = input("Enter a name: ")
+    print("---------------------------")
+    wage = eval(input("Enter a wage: "))
+    print("---------------------------")
+    hours = eval(input("Enter hours: "))
+    print("---------------------------")
+    #equations for information inputed
+    overtimehours = hours - 40
+    if overtimehours < 0:
+        overtimehours = 0
+    regularwage = hours * wage
+    overtimewage = (wage * 1.5) * overtimehours
+    tax = (regularwage + overtimewage) * 0.20
+    medical = (regularwage + overtimewage) * .10
+    print()
+    print()
+    #printing out all above equations
+    print("Name: \t\t\t", name)
+    print()
+    print("Hours: \t\t\t", hours)
+    print()
+    print("Wage: \t\t\t", wage)
+    print()
+    print("Regular Wages: \t\t", regularwage )
+    print()
+    print("Overtime Wages: \t", overtimewage)
+    print()
+    print("Total Wages: \t\t", regularwage + overtimewage)
+    print()
+    print("Tax Withholding: \t", tax)
+    print()
+    print("Medical Withholding: \t", medical)
+    print()
+    print("Take Home Pay: \t\t", (regularwage + overtimewage) - (tax + medical))  
+    #print("Take Home:", take home )
+   
 
 
 main()
--- Prob-3/Prob-3.py
+++ Prob-3/Prob-3.py
@@ -2,15 +2,38 @@
 #   Programming Assignment 4
 #     Prob-3.py
 
-# <YOUR NAME>
+# Jonathan Ciarlo
 
 def letterGrade(score):
     # your code here
-
+    grade = "inavalid"
+   
+   
+    if score == 0:
+        grade = "F"
+    elif score == 1:
+        grade = "F"
+    elif score == 2:
+        grade = "D"
+    elif score == 3:
+        grade = "C"
+    elif score == 4:
+        grade = "B"
+    elif score == 5:
+        grade = "A"
+       
     return grade
+    
 
 def unitTest():
     # your test code here
+    print("Leter grade:", letterGrade(0))
+    print("Leter grade:", letterGrade(1))
+    print("Leter grade:", letterGrade(2))
+    print("Leter grade:", letterGrade(3))
+    print("Leter grade:", letterGrade(4))
+    print("Leter grade:", letterGrade(5))
+
 
 if __name__ == "__main__":
     unitTest()
