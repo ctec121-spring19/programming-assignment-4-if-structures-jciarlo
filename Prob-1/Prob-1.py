b919daf93a262746b6e1248cbd7b8df114d7be2f
Jon Ciarlo <j.ciarlo@students.clark.edu>
2019-05-04 09:29:42 -0700

    Prob 1 finished

--- Prob-1/Prob-1.py
+++ Prob-1/Prob-1.py
@@ -2,19 +2,21 @@
 #   Programming Assignment 4
 #     Prob-1.py
 
-# <YOUR NAME>
+# Jonathan Ciarlo
 
 def shippingCost(orderSubTotal):
     shippingCost = 2.99
     # enter code here to test for free
-
+    if orderSubTotal >= 10.00:
+        shippingCost = 0.00
 
     return shippingCost
 
 def unitTest():
-    print("Shipping cost if subtotal < 10.00:\t", shippingCost(5.99))
+    print("Expected value: 2.99 - Shipping cost if subtotal < 10.00:\t", shippingCost(5.99))
     # enter additional test code here
-
+    print("Expected value: 0.0 - Shiping cost if subtotal > 10.00:\t", shippingCost(11.00))
+    print("Expected value: 0.0 - Shipping cost if subtotal = 10.00:\t", shippingCost(10.00))
 
 if __name__ == "__main__":
     unitTest()
